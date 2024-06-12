224f995505fa543ffea2e62ad4806e19d77c4640
TakedaKaho <tktk.a3104c@gmail.com>
2024-06-12 11:30:01 +0000

    java 条件式と繰り返し処理

---
+++ app.js
@@ -0,0 +1,22 @@
+let hello = 'Hello World';
+alert(hello);
+
+// 整数を代入する
+//let int1 = 1;
+
+// 負数を代入する
+//let int2 = -10;
+
+// 小数点を代入する
+//let float1 = 3.14;
+
+// 文字列を代入する
+//let str1 = 'JavaScriptを覚えよう';
+let i;
+let num = 0;
+
+for(i = 1; i < 11; i++){
+  num = num + i;
+}
+
+alert('1から10まで足し算した結果は' + num + 'です');
